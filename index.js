7b1cd39377cb8a003b5baa1dccfa07950ea7be47
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-26 23:17:34 +0000

    created a login form

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function submitForm() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log("User:", username);
+    console.log("Pass:", password);
+    axios.post("http://localhost:3555/login",
+    { username, password})
+    .then((response) => response.data)
+    .then((data) => {
+        const jwtToken = data.token;
+        localStorage.setItem("jwtToken", jwtToken);
+        console.log("Login working!", jwtToken);
+    })
+}
 
+function logout() {
+    localStorage.removeItem("jwtToken");
+}
